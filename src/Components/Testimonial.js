a9a1882455da52baccf4212dfd6f8375de2d9f92
sahill-1 <sv260397@gmail.com>
2023-11-30 10:57:29 +0530

    homepage completed

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-
 const testimonialsData = [
   {
     id: 1,
@@ -29,46 +28,45 @@ const testimonialsData = [
 ];
 
 const Testimonial = () => {
-  const [index, setIndex] = useState(0);
 
-  const handleNext = () => {
-    setIndex((state) =>
-      state === testimonialsData.length - 1 ? 0 : state + 1
-    );
-  };
-
-  const handlePrev = () => {
-    setIndex((state) =>
-      state === 0 ? testimonialsData.length - 1 : state - 1
-    );
-  };
+const [index, setIndex] = useState(0);
+const handleNext = () => {
+  setIndex((state) =>
+    state === testimonialsData.length - 1 ? 0 : state + 1
+  );
+};
+const handlePrev = () => {
+  setIndex((state) =>
+    state === 0 ? testimonialsData.length - 1 : state - 1
+  );
+};
 
   return (
     <>
-      <div className="mt-5 text-center new-arrival">
-        <h2>Testimonial</h2>
-      </div>
-      <div className="testimonial-slider ">
-        <div className="testimonial">
-          <div className="testimonial-content">
-            <p className="quote">{testimonialsData[index].quote}</p>
-            <div className="author-info">
-              <img src={testimonialsData[index].img} alt={testimonialsData[index].author}/>
-              <h4>{testimonialsData[index].author}</h4>
-              <p>{testimonialsData[index].position}</p>
-            </div>
-          </div>
-        </div>
-        <div className="controls">
-          <div className="arrow arrow-left" onClick={handlePrev}>
-            <FaChevronRight />
-          </div>
-          <div className="arrow arrow-right" onClick={handleNext}>
-            <FaChevronLeft />
+    <div className="mt-5 text-center new-arrival">
+      <h2>Testimonial</h2>
+    </div>
+    <div className="testimonial-slider ">
+      <div className="testimonial">
+        <div className="testimonial-content">
+          <p className="quote">{testimonialsData[index].quote}</p>
+          <div className="author-info">
+            <img src={testimonialsData[index].img} alt={testimonialsData[index].author}/>
+            <h4>{testimonialsData[index].author}</h4>
+            <p>{testimonialsData[index].position}</p>
           </div>
         </div>
       </div>
-    </>
+      <div className="controls">
+        <div className="arrow arrow-left" onClick={handlePrev}>
+          <FaChevronRight />
+        </div>
+        <div className="arrow arrow-right" onClick={handleNext}>
+          <FaChevronLeft />
+        </div>
+      </div>
+    </div>
+  </>
   );
 };
 
