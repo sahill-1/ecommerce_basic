67fefc529ff3cbb0a28489dd72c6a918c2563996
sahill-1 <sv260397@gmail.com>
2023-12-06 09:51:46 +0530

    contact us page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import AboutUs from "./Pages/AboutUs"
 import ContactUs from './Pages/ContactUs';
+import Register from "./Pages/Register";
 function App() {
   return (
     <>
@@ -11,6 +12,8 @@ function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='/aboutus' element={<AboutUs />} />
         <Route path='/contactus' element={<ContactUs />} />
+        <Route path='/register' element={<Register />} />
+
       </Routes>
     </Router>    
     </>
